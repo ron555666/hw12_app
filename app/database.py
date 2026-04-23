5768277b85d75a98d43305c2032a504a3bcbd817
ron555666 <165968242+ron555666@users.noreply.github.com>
2026-04-23 16:40:53 -0700

    fix db config and tests

--- app/database.py
+++ app/database.py
@@ -1,21 +1,19 @@
 import os
-from sqlmodel import SQLModel, Session, create_engine
-from dotenv import load_dotenv
+from sqlmodel import create_engine, Session
 
-load_dotenv()
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/hw12_app"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/hw12_app"
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
 )
-
-engine = create_engine(DATABASE_URL, echo=True)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 
 def get_session():
     with Session(engine) as session:
         yield session
+        # FastAPI pattern
+
+        # return session
--- app/database_async.py
+++ app/database_async.py
@@ -1,26 +1,14 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlmodel import Session
 
-load_dotenv()
+DATABASE_URL="postgresql+asyncpg://postgres:postgres@localhost:5432/hw12_app"
 
-ASYNC_DATABASE_URL = os.getenv(
-    "ASYNC_DATABASE_URL",
-    "postgresql+asyncpg://postgres:postgres@localhost:5432/hw12_app"
-)
-
-async_engine = create_async_engine(
-    ASYNC_DATABASE_URL,
+engine = create_async_engine(
+    DATABASE_URL,
     echo=True
-)
-
-AsyncSessionLocal = async_sessionmaker(
-    bind=async_engine,
-    class_=AsyncSession,
-    expire_on_commit=False
 )
 
 
 async def get_async_session():
-    async with AsyncSessionLocal() as session:
+    async with AsyncSession(engine) as session:
         yield session
